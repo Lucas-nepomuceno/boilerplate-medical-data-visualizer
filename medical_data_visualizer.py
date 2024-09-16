12030387f74223a55a80bcebf35b2323cfd420af
Lucas-nepomuceno <158762017+Lucas-nepomuceno@users.noreply.github.com>
2024-09-16 11:39:20 -0300

    feat: adds medical_data_visualizer

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df['weight'] / (df['height']/100) ** 2  
+df.loc[df['overweight'] <= 25, 'overweight'] = 0
+df.loc[df['overweight'] > 25, 'overweight'] = 1
 
 # 3
+df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
 
+df.loc[df['gluc'] == 1, 'gluc'] = 0
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+
 
     # 7
-
+    graph = sns.catplot(x='variable', hue='value', col='cardio', data=df_cat, kind='count')
 
 
     # 8
-    fig = None
-
+    graph.set_axis_labels("variable", "total")
+    fig = graph.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -38,21 +44,35 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975)) &
+        (df['ap_lo'] <= df['ap_hi'])
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
+
 
     # 15
-
+    sns.heatmap(corr, 
+            mask=mask,  # You can comment this out if you don't want to mask the upper triangle
+            annot=True, 
+            fmt=".1f", 
+            cmap="coolwarm",  # Use the coolwarm palette to match the "red" style
+            square=True, 
+            ax=ax, 
+            cbar_kws={"shrink": .5})
 
 
     # 16
